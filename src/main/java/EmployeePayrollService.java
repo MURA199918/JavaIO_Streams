e9b24dd234653a6c609a953b8672c535b62d904c
MURA199918 <muralisaranr18@gmail.com>
2020-12-07 15:45:36 +0530

    Added UC4 to store employee payroll into file

--- src/main/java/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService.java
@@ -1,15 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Employee ID: ");
-        int id = sc.nextInt();
-        System.out.println("Enter Employee Name: ");
-        String name = sc.next();
-        System.out.println("Enter Employee Salary: ");
-        double salary = sc.nextDouble();
-        System.out.println("Employee Details: "+id+" "+name+" "+salary);
+    public enum IOService {CONSOLE_IO, FILE_IO, DB_IO, REST_IO}
+    private List<EmployeePayrollData> employeePayrollList;
 
+    public EmployeePayrollService(){}
+
+    public EmployeePayrollService(List<EmployeePayrollData>
+                                  employeePayrollList){}
+    public static void main(String[] args) {
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData(IOService.CONSOLE_IO);
+
+    }
+
+    public void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID: ");
+        int id = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name: ");
+        String name = consoleInputReader.next();
+        System.out.println("Enter Employee Salary: ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id,name,salary));
+    }
+
+    public void writeEmployeePayrollData(EmployeePayrollService.IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("Writing Employee Payroll Roaster to Console "+employeePayrollList);
+        else if(ioService.equals(IOService.FILE_IO))
+            new EmployeePayrollFileIOService().writeData(employeePayrollList);
     }
 }
---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries(){
+        EmployeePayrollData[] arrayofEmps = {
+                new EmployeePayrollData(1,"Jeff Bezos",100000.0),
+                new EmployeePayrollData(2,"Bill Gates",200000.0),
+                new EmployeePayrollData(3,"Mark Zuckerberg",300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayofEmps));
+        employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+    }
+}

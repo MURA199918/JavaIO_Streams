886397599a12a5562709142d436640cccd462801
MURA199918 <muralisaranr18@gmail.com>
2020-12-18 15:14:46 +0530

    [Murali]Refactored to create payroll service database and connect java program to DB

--- src/main/java/EmployeePayrollDBService.java
+++ src/main/java/EmployeePayrollDBService.java
@@ -16,11 +16,10 @@ public class EmployeePayrollDBService {
         return connection;
     }
 
-    public List<EmployeePayrollData> readData() throws SQLException {
+    public List<EmployeePayrollData> readData() {
         String sql = "SELECT * FROM employee_payroll";
         List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        try{
-            Connection connection = this.getConnection();
+        try(Connection connection = this.getConnection()){
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()){
